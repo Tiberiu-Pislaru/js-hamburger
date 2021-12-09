78dc1bc0355bfd36fdcb507762e721e3fe09c79e
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2021-12-09 17:28:01 +0100

    Esercizio concluso

---
+++ main.js
@@ -0,0 +1,16 @@
+const open = document.querySelector('.fa-bars');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const close = document.querySelector('.close');
+// let trackerClick= 0;
+
+// console.log(hamburgerMenu);
+open.addEventListener('click', function() {
+    hamburgerMenu.classList.add("active");
+    // console.log('ciao')
+})
+
+close.addEventListener('click', function() {
+    hamburgerMenu.classList.remove("active");
+    // console.log('ciao')
+})
+
