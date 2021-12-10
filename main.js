19dd7adb693b9a152a767d4df7273f9a8fd4cc44
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2021-12-10 09:55:58 +0100

    .

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ const close = document.querySelector('.close');
 open.addEventListener('click', function() {
     hamburgerMenu.classList.add("active");
     // console.log('ciao')
-})
+});
 
 close.addEventListener('click', function() {
     hamburgerMenu.classList.remove("active");
     // console.log('ciao')
-})
+});
 
